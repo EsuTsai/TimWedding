4b6aba6e3e869503c1d6d3da1a1628a85e39798d
Esu <esutsai@gmail.com>
2015-11-20 01:46:41 +0800

    ios9 fix

--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, "8.0"
 pod "pop", "1.0.7"
 pod "AFNetworking", "2.5.3"
-pod "SDWebImage", "3.7.1"
+pod "SDWebImage", "3.7.3"
 pod "DACircularProgress", "2.2.0"
 pod "FontAwesomeKit", "~> 2.1.0"
 pod "Parse"
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -51,8 +51,8 @@
 // Bolts/Tasks
 #define COCOAPODS_POD_AVAILABLE_Bolts_Tasks
 #define COCOAPODS_VERSION_MAJOR_Bolts_Tasks 1
-#define COCOAPODS_VERSION_MINOR_Bolts_Tasks 2
-#define COCOAPODS_VERSION_PATCH_Bolts_Tasks 2
+#define COCOAPODS_VERSION_MINOR_Bolts_Tasks 3
+#define COCOAPODS_VERSION_PATCH_Bolts_Tasks 0
 
 // DACircularProgress
 #define COCOAPODS_POD_AVAILABLE_DACircularProgress
@@ -124,13 +124,13 @@
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
 #define COCOAPODS_VERSION_MAJOR_SDWebImage 3
 #define COCOAPODS_VERSION_MINOR_SDWebImage 7
-#define COCOAPODS_VERSION_PATCH_SDWebImage 1
+#define COCOAPODS_VERSION_PATCH_SDWebImage 3
 
 // SDWebImage/Core
 #define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
 #define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
 #define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
-#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 1
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 3
 
 // pop
 #define COCOAPODS_POD_AVAILABLE_pop
